22f129c9b90640d59f9ff87ca9bc81cba0a3c409
vimal772 <vimal115077286@gmail.com>
2023-10-01 17:55:39 +0530

    Script for the game is completed the game is running

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 console.log("connected");
+const str = ["rock","scissor","paper"];
+let choice;
+let userChoice;
+
+function getComputerChoice(){
+    let c = Math.floor(Math.random() * 3);
+    choice = str[c];
+    console.log("Bot's choice "+choice)
+    return choice;
+}
+
+function getUserChoice(){
+    alert("choose only from rock,paper,scissor");
+    userChoice = prompt("Enter the value ");
+    userChoice = userChoice.toLowerCase();
+    console.log("Your Choice "+userChoice)
+    return userChoice; 
+}
+
+function checkLogic(choice,userChoice){
+    if(userChoice === "rock"){
+        if(choice === "rock"){
+            console.log("Draw Due to Same choice selected");
+        }else if(choice === "paper"){
+            console.log("Bot Wins by Choosing Paper");
+        }else{
+            console.log("User Wins Due to Bot's scissor choice");
+        }
+    }
+    else if(userChoice === "paper"){
+        if(choice === "paper"){
+            console.log("Draw Due to Same choice selected");
+        }else if(choice === "scissor"){
+            console.log("Bot Wins by Choosing scissor");
+        }else{
+            console.log("User Wins Due to Bot's rock choice");
+        }
+    }
+    if(userChoice === "scissor"){
+        if(choice === "scissor"){
+            console.log("Draw Due to Same choice selected");
+        }else if(choice === "rock"){
+            console.log("Bot Wins by Choosing rock");
+        }else{
+            console.log("User Wins Due to Bot's paper choice");
+        }
+    }
+}
+
+function game(){
+    getComputerChoice();
+    getUserChoice();
+    checkLogic(choice,userChoice);
+}
+
+game();
